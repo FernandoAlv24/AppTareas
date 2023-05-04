bd9f9e30701aee76a2df4fc647d359a1b514e4b9
FernandoAlv24 <100179088+FernandoAlv24@users.noreply.github.com>
2023-05-04 18:09:18 -0500

    agregar lista de tareas

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Tarea } from 'src/app/models/tarea';
 @Component({
   selector: 'app-tareas',
   templateUrl: './tareas.component.html',
   styleUrls: ['./tareas.component.css']
 })
 export class TareasComponent {
+listTareas:Tarea[]=[]
+nombreTarea='';
 
+constructor(){}
+
+ngOnInit():void{}
+
+agregarTarea(){
+  // crear un objeto tarea
+const tarea:Tarea={
+  nombre:this.nombreTarea,
+  estado:false
+}
+
+  //agregar objeto al array
+this.listTareas.push(tarea);
+
+  //resetear form
+  this.nombreTarea = '';
+}
 }
